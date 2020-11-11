ba8757dd6ac8c6ea8ab85b3201af089eb4777688
franksterge <fangwg@uw.edu>
2020-11-10 20:21:43 -0800

    cleaned up with gitignore

--- README.md
+++
@@ -1,2 +0,0 @@
-# INFO 5100 Assignments
-Welcome to Fangwen Ge's repository for INFO 5100 at Northeastern University!
--- assignment6/src/interfaces/Boardable.java
+++
@@ -1,7 +0,0 @@
-package interfaces;
-
-public interface Boardable {
-    void setBoardStart(int month, int day, int year);
-    void setBoardEnd(int month, int day, int year);
-    boolean boarding(int month, int day, int year);
-}
